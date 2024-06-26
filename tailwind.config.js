b1a9a2781fbc44db0a8e19fb4ed8da59e4c90eba
Parth Prajapati <parth6703@gmail.com>
2024-06-26 16:53:15 +0530

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.html"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'bg_img': "url('./images/bg.png')",
+
+      }, colors: {
+        parth: '#E6E4E4',
+        text_color: '#254D4D',
+        text_color2: '#14141499',
+        bg_color: '#254D4D1A',
+        bg_color2: '#254D4D',
+        bg_color3: '#F5F5F5',
+        bg_color4: '#FCF8F2',
+        bg_color5: '#F1DDC1',
+        text_p: '#313131',
+        btn_col: '#7AA9A7'
+      }, fontFamily: {
+        'Roboto': ['Roboto', 'sans-serif'],
+        'Gujarati' :['Noto Sans Gujarati', 'sans-serif'],
+        'Montserrat':['Montserrat', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
+
